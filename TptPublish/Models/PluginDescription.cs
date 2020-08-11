a8f8857d03c31f60d36865601df53f7afb7f0b97
Mark-Anthony Hutton <mark-anthony.hutton@opsysinc.com>
2020-08-11 10:50:05 -0600

    DT502: New publish project created. Initial logic added for publishing TPT.

---
+++ TptPublish/Models/PluginDescription.cs
@@ -0,0 +1,83 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace PpmMain.Models
+{
+    /// <summary>
+    /// The description of an installed plugin
+    /// </summary>
+    public class PluginDescription
+    {
+        /// <summary>
+        /// The plugin name.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// A unique shortname representing the plugin.
+        /// </summary>
+        public string ShortName { get; set; }
+
+        /// <summary>
+        /// The version of the plugin.
+        /// </summary>
+        public string Version { get; set; }
+
+        /// <summary>
+        /// A description of the plugin.
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// A description of what has changed in this version of the plugin.
+        /// </summary>
+        public string VersionDescription { get; set; }
+
+        /// <summary>
+        /// The versions of ParaText that this plugin version is compatible with.
+        /// </summary>
+        public List<string> PtVersions { get; set; }
+
+        /// <summary>
+        /// The license for this plugin.
+        /// </summary>
+        public string License { get; set; }
+
+        public override string ToString()
+        {
+            return JsonConvert.SerializeObject(this, Formatting.Indented);
+        }
+
+        /// <summary>
+        /// This function creates a <c>PluginDescription</c> from a JSON file.
+        /// </summary>
+        /// <param name="pluginDescriptionFilePath">The <c>PluginDescription</c> JSON filepath to read from. (required)</param>
+        /// <returns>The deserialized <c>PluginDescription</c></returns>
+        public static PluginDescription FromFile(string pluginDescriptionFilePath)
+        {
+            // validate inputs
+            _ = pluginDescriptionFilePath ?? throw new ArgumentNullException(nameof(pluginDescriptionFilePath));
+
+            string rawPluginDescription = File.ReadAllText(pluginDescriptionFilePath);
+            PluginDescription pluginDescription = JsonConvert.DeserializeObject<PluginDescription>(rawPluginDescription);
+
+            return pluginDescription;
+        }
+
+        /// <summary>
+        /// This function saves a <c>PluginDescription</c> to a JSON file.
+        /// </summary>
+        /// <param name="pluginDescriptionFilePath">The <c>PluginDescription</c> JSON filepath to save to. (required)</param>
+        public void SaveToFile(string pluginDescriptionFilePath)
+        {
+            // validate inputs
+            _ = pluginDescriptionFilePath ?? throw new ArgumentNullException(nameof(pluginDescriptionFilePath));
+
+            // serialize JSON to a string and then write string to a file
+            File.WriteAllText(pluginDescriptionFilePath, this.ToString());
+        }
+    }
+}
