82a68a50c76223a949657f4907e656d9dcd202d5
Mark-Anthony Hutton <mark-anthony.hutton@opsysinc.com>
2020-08-14 11:17:19 -0600

    DT502: Fixed incorrect reference to script root directory.

--- Scripts/PublishTptToPpm.bat
+++ Scripts/PublishTptToPpm.bat
@@ -2,7 +2,7 @@ REM A batchscript to deploy the Paratext plugin to the Paratext Plugin Manager.
 
 REM Set function variables
 SET SCRIPT_ROOT=%~dp0
-SET SOLUTON_ROOT=%SCRIPT_DIR%..
+SET SOLUTON_ROOT=%SCRIPT_ROOT%..
 
 REM Run the publish executable
 cd %SOLUTON_ROOT%\TptPublish\
