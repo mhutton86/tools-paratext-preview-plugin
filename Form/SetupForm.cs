870db48f7a4c44d7840c4d6e3fb6f428091c53c0
Rico Alire <rico.alire@opsysinc.com>
2021-08-20 17:11:43 -0600

    DT-240: Add the support URL to the help drop down and add the support URL to the constants

--- Form/SetupForm.cs
+++ Form/SetupForm.cs
@@ -7,6 +7,7 @@ using Paratext.Data.ProjectSettingsAccess;
 using TptMain.Properties;
 using System.Reflection;
 using System.Drawing;
+using System.Diagnostics;
 
 namespace TptMain.Form
 {
@@ -546,6 +547,18 @@ namespace TptMain.Form
             
             // Return the list of books specified by the user
             return tbCustomBookSet.Text.Trim().Replace(" ", "");
+        }
+
+        /// <summary>
+        /// Opens a link to the support URL from the plugin
+        /// </summary>
+        /// <param name="sender">The control that sent this event</param>
+        /// <param name="e">The event information that triggered this call</param>
+        private void contactSupportToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //Call the Process.Start method to open the default browser
+            //with a URL:
+            Process.Start(MainConsts.SUPPORT_URL);
         }
     }
 }
